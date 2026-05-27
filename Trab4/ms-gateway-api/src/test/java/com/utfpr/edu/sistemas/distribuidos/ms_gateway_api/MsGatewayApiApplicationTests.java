cbca6225578ef2cfbec6c9e94d4885e3193aa827
franciscoldb <fluis905@gmail.com>
2026-05-27 16:38:08 -0300

    Trab4: add ms-gateway-api creation

---
+++ Trab4/ms-gateway-api/src/test/java/com/utfpr/edu/sistemas/distribuidos/ms_gateway_api/MsGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utfpr.edu.sistemas.distribuidos.ms_gateway_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
